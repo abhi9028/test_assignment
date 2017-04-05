3d8b142be03ddaa35cc3f095f052844386da22aa
Abhinav Srivastav <abhinav.fun90@gmail.com>
2017-04-05 08:44:41 +0530

    changes in gemfile

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'paperclip', '~> 4.3'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
+gem 'rails_12factor', group: :production
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ActiveRecord::Base
 	has_attached_file :avatar, styles: { medium: "270x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-
   validates :name, presence: true
 
   def timestamp
